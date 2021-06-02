bca7de7eafc5418908b3b23daa1c54606e32f76d
Roberto <mitras25jpn@hotmail.com>
2021-06-01 21:44:39 -0300

    Cod3r angular

---
+++ cod3r/angular9/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { HomeComponent } from './views/home/home.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+},
+{
+  path: "products",
+  component: ProductCrudComponent
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
